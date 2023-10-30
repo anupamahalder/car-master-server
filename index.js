51481d9b9b8211bf56ce7a6b44b4140473921fc4
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-30 13:18:39 +0530

    Added a get api to get services using id and send selective property using projection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -45,7 +45,17 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     })
-
+    // create api to get a specific service from database 
+    app.get('/services/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const options = {
+          // if we want some property then write 1 else 0 and by default _id will be provided 
+          projection: {title: 1, price: 1 ,service_id:1},
+        };
+        const result = await serviceCollection.findOne(query,options);
+        res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
